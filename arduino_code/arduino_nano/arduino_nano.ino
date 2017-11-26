225d313c5bc8e28c19f9f0b4e6f83be225888484
jcorcoran <james@team2168.org>
2017-11-25 20:05:45 -0500

    Move the pins up one to avoid having the enable signal on pin 13. This pin has an LED on it on some boards which appears to affect the state of the enable signal on power up in some cases.

--- arduino_code/arduino_nano/arduino_nano.ino
+++ arduino_code/arduino_nano/arduino_nano.ino
@@ -10,12 +10,12 @@ typedef struct {
 } sig_type;
 
 sig_type signals[] = {                 //ENABLE SIGNAL MUST BE LAST!
-  {.input_pin = 8,  .output_pin =  4}, //RX Ch1 - Shooter Speed
-  {.input_pin = 9,  .output_pin =  2}, //RX Ch2 - Right DT Motor Speed
-  {.input_pin = 10, .output_pin =  3}, //RX Ch3 - Left DT Motor Speed
-  {.input_pin = 11, .output_pin =  5}, //RX Ch4 - Raise Solenoid
-  {.input_pin = 12, .output_pin =  6}, //RX Ch5 - Fire Solenoid
-  {.input_pin = 13, .output_pin = -1}  //RX Ch6 - Robot Enable/Disable (no output pin)
+  {.input_pin = 7,  .output_pin =  4}, //RX Ch1 - Shooter Speed
+  {.input_pin = 8,  .output_pin =  2}, //RX Ch2 - Right DT Motor Speed
+  {.input_pin = 9,  .output_pin =  3}, //RX Ch3 - Left DT Motor Speed
+  {.input_pin = 10, .output_pin =  5}, //RX Ch4 - Raise Solenoid
+  {.input_pin = 11, .output_pin =  6}, //RX Ch5 - Fire Solenoid
+  {.input_pin = 12, .output_pin = -1}  //RX Ch6 - Robot Enable/Disable (no output pin)
 };
 const int num_signals = sizeof(signals) / sizeof(sig_type);
 const int enable_signal = num_signals - 1;
